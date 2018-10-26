cc235f31d7c27e34e7cccd2a4a0e6357804c0320
Haodong Yang <daniel.aws.certificate@gmail.com>
2018-10-25 20:58:35 -0400

    finish queue

--- queue/linked_queue.py
+++ queue/linked_queue.py
@@ -17,3 +17,41 @@ class LinkedQueue(object):
     def enqueue(self, value):
         new_node = Node(value)
         if self._tail is None:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            self._tail.next = new_node
+            self._tail = new_node
+            
+    def dequeue(self):
+        if self._head is None:
+            return False
+        value = self._head.data
+        self._head = self._head.next
+        if self._head is None:
+            self._tail = None
+        return value
+
+    def __str__(self):
+        values = []
+        current = self._head
+        while current:
+            values.append(current.data)
+            current = current.next
+        return ' '.join([value for value in values])
+
+
+if __name__ == "__main__":
+    q = LinkedQueue()
+    for i in range(10):
+        q.enqueue(str(i))
+    print(q)
+
+    for _ in range(3):
+        q.dequeue()
+    print(q)
+
+    q.enqueue("7")
+    q.enqueue("8")
+    print(q)
+
